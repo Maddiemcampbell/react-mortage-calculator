4837e78884889510d642f956dfb60f66e8478d84
Maddie <maddiemcampbell@gmail.com>
2019-09-21 14:15:08 -0700

    working calculator and page title

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -6,7 +6,7 @@ export default class App extends React.Component {
     this.state = {
       rate: 0,
       balance: 0,
-      term: 0,
+      term: 15,
       submit: 0,
     }
     this.handleInputChange=this.handleInputChange.bind(this);
@@ -28,21 +28,31 @@ export default class App extends React.Component {
   render() {
     return (
       <div className='container'>
-        <div class="form-group">
-          <label className="col-sm-2 control-label">Mortgage Balance</label>
-          <input name='balance' type ='number' value={this.state.balance} onChange={this.handleInputChange}></input>
-          <label className="col-sm-2 control-label">Mortgage Rate</label>
-          <input name ='rate' type='number' step='0.01' value={this.state.rate} onChange={this.handleInputChange}></input>
-          <select name='term' value={this.state.term} onChange={this.handleInputChange}>
-            <option value='15'>15</option>
-            <option value='30'>30</option>
-          </select>
-          <button name='submit' value={this.state.submit} onChange={this.handleInputChange} onClick={this.calculate}>Submit</button>
-        </div>
-        <div name='output' id='output'>
-          {this.state.submit} is your payment.
-        </div>
+      
+      <h2>Mortgage Calculator</h2>
+      
+      <div class="form-group">
+        <label className="col-sm-2 control-label">Mortgage Balance</label>
+        
+        <input name='balance' type ='number' value={this.state.balance} onChange={this.handleInputChange}></input>
+        
+        <label className="col-sm-2 control-label">Mortgage Rate</label>
+        
+        <input name ='rate' type='number' step='0.01' value={this.state.rate} onChange={this.handleInputChange}></input>
+        <select name='term' value={this.state.term} onChange={this.handleInputChange}>
+          <option value='15'>15</option>
+          <option value='30'>30</option>
+        </select>
+       
+        <button name='submit' value={this.state.submit} onChange={this.handleInputChange} onClick={this.calculate}>Submit</button>
+      
       </div>
+      
+      <div name='output' id='output'>
+        {this.state.submit} is your payment.
+      </div>
+    
+    </div>    
     );
   }
 }
