2371d2f9a33a2ef070f334b4dff5b518fde0132a
Maddie <maddiemcampbell@gmail.com>
2019-09-21 12:48:12 -0700

    handles state change

--- src/js/app.jsx
+++ src/js/app.jsx
@@ -9,18 +9,24 @@ export default class App extends React.Component {
       term: 0,
       submit: 0,
     }
+    this.handleInputChange=this.handleInputChange.bind(this);
   }
+
+  handleInputChange (e) {
+    this.setState({ [e.target.name] : e.target.value});
+  }
+
   render() {
     return (
       <div className='container'>
         <div class="form-group">
-          <input name='balance' type ='number' value={this.state.balance}></input>
-          <input name ='rate' type='number' step='0.01' value={this.state.rate}></input>
-          <select name='term' value={this.state.term}>
+          <input name='balance' type ='number' value={this.state.balance} onChange={this.handleInputChange}></input>
+          <input name ='rate' type='number' step='0.01' value={this.state.rate} onChange={this.handleInputChange}></input>
+          <select name='term' value={this.state.term} onChange={this.handleInputChange}>
             <option value='15'>15</option>
             <option value='30'>30</option>
           </select>
-          <button name='submit' value={this.state.submit}>Submit</button>
+          <button name='submit' value={this.state.submit} onChange={this.handleInputChange}>Submit</button>
         </div>
         <div name='output' id='output'>
 
